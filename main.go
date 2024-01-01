d9abe224c5232aac3b19db6c23b23ef06ebd0555
Rafael Miranda Durante <rafaelmdurante@gmail.com>
2024-01-01 17:39:13 +0000

    feat: add simple contact page

--- main.go
+++ main.go
@@ -5,15 +5,26 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	_, err := fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 	if err != nil {
 		return
 	}
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, err := fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, "+
+		"email me at <a href=\"mailto:my@email.com\">my@email.com</a>.")
+	if err != nil {
+		return
+	}
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("starting server on :3000...")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
