59bc0007882459db824fd8cc2076d60d3de189c5
Rafael Miranda Durante <rafaelmdurante@gmail.com>
2024-01-16 16:44:26 +0000

    experiment: database connection experiments

---
+++ cmd/exp/exp_db.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (c PostgresConfig) String() string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		c.Host, c.Port, c.User, c.Password, c.Database, c.SSLMode)
+}
+
+func main() {
+	c := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "baloo",
+		Password: "junglebook",
+		Database: "lenslocked",
+		SSLMode:  "disable",
+	}
+
+	db, err := sql.Open("pgx", c.String())
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected!")
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	email TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+	id SERIAL PRIMARY KEY,
+	user_id INT NOT NULL,
+	amount INT,
+	description TEXT
+);`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Tables created.")
+
+	name := "Robert Smith"
+	email := "singer@thecure.com"
+
+	var id int
+
+	err = db.QueryRow(
+		` INSERT INTO users(name, email) VALUES ($1, $2) RETURNING id;`,
+		name, email).Scan(&id)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("User id '%v' created.\n", id)
+}
