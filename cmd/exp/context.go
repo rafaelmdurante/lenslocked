9cefbfe432063e4bb813d02159dbd490295e241b
Rafael Miranda Durante <rafaelmdurante@gmail.com>
2024-03-22 09:37:43 +0000

    exp: create custom type to use with context

--- cmd/exp/context.go
+++ cmd/exp/context.go
@@ -5,13 +5,27 @@ import (
 	"fmt"
 )
 
+// make it private so only code within your package can use this
+type ctxKey string
+
+const (
+    // make it private so only code within your package can use this
+    favouriteColour ctxKey = "favourite-colour"
+)
+
 func main() {
     // get an empty context
     ctx := context.Background()
-    // override it with a new value
-    ctx = context.WithValue(ctx, "favourite-colour", "blue")
+
+    // having a custom type of ctxKey makes it different from the same "key" of
+    // pure string
+    ctx = context.WithValue(ctx, favouriteColour, "blue")
+    ctx = context.WithValue(ctx, "favourite-colour", "red")
+
     // get the value from the context
-    v := ctx.Value("favourite-colour")
-    fmt.Println(v)
+    fmt.Println(
+        ctx.Value(favouriteColour),
+        ctx.Value("favourite-colour"),
+    )
 }
 
