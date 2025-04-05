6b6a8accceb66506cffe149f440430e6557aaad9
Rafael Miranda Durante <rafaelmdurante@gmail.com>
2025-04-05 07:01:12 +0100

    feat: create galleries controller to render template
    section 21: new gallery handler

---
+++ controllers/galleries.go
@@ -0,0 +1,23 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/rafaelmdurante/lenslocked/models"
+)
+
+type Galleries struct {
+	Templates struct {
+		New Template
+	}
+	GalleryService *models.GalleryService
+}
+
+func (g Galleries) New(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Title string
+	}
+	data.Title = r.FormValue("title")
+	g.Templates.New.Execute(w, r, data)
+}
+
